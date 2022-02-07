4e7601cc22fa344598bf89f0b59c9d573dbc26b8
Cecil Hickam <liamh1@vt.edu>
2022-02-06 19:03:51 -0500

    File for cleaning spectral dataset

---
+++ hbSoils/SpectralConductivity.R
@@ -0,0 +1,22 @@
+#Spectral conditions cleaning
+
+#necessary packages
+library(lubridate)
+library(tidyverse)
+library(ggplot2)
+
+
+#load in csv file
+spectralConditionsRaw <- read_csv("hbSoils/data_raw/HBF Spec Cond.csv")
+
+#Select Necessary Columns
+cleanSpectral <- spectralConditionsRaw %>% select(Date.Time.EST, SpConductivity)
+
+
+#Mutate to posix Type
+cleanSpectral <- cleanSpectral %>% mutate(timeSeries = mdy_hm(Date.Time.EST)) %>%
+  select(timeSeries, SpConductivity)
+
+
+#Test Plot
+ggplot(cleanSpectral, aes(x=timeSeries, y = SpConductivity)) + geom_line()
