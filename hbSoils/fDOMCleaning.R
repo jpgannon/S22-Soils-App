1c1120dfe3d07ca4c685c324037633d2af6d20dd
Cecil Hickam <liamh1@vt.edu>
2022-02-09 13:22:54 -0500

    fDOM file cleaning

---
+++ hbSoils/fDOMCleaning.R
@@ -0,0 +1,21 @@
+library(ggplot2)
+library(tidyverse)
+library(lubridate)
+
+#Import data
+
+flurDataRaw <- read_csv("hbSoils/data_raw/HBF fDOM.csv")
+
+#Clean Data
+
+cleanFlurData <- flurDataRaw %>% select(Date.Time.EST, FDOM_corrected_QSU)
+
+#Mutate to DateTime
+cleanFlurData <- cleanFlurData %>% mutate(timeSeries = mdy_hm(Date.Time.EST)) %>%
+  select(timeSeries, FDOM_corrected_QSU)
+
+#Test GGPlot
+ggplot(cleanFlurData, aes(timeSeries, FDOM_corrected_QSU)) + geom_line() +
+  theme_classic()
+
+
