edc8030904be9151d90a2d7a013bb0b8e6806509
Cecil Hickam <liamh1@vt.edu>
2022-02-16 13:56:03 -0500

    updated Server

--- hbSoils/server.R
+++ hbSoils/server.R
@@ -6,7 +6,16 @@
 #
 #    http://shiny.rstudio.com/
 #
+library(lubridate)
+library(ggplot2)
+library(tidyverse)
 
+
+temperatureRawData <- read_csv("hbSoils/data_raw/HBF Temperature.csv")
+
+
+clean_temperature_data <- temperatureRawData %>% mutate(date = mdy_hm(Date.Time.EST)) %>%
+  select(date, TempC)
 
 
 # Define server logic required to draw a histogram
@@ -14,7 +23,7 @@ shinyServer(function(input, output) {
 
     
     output$timePlot <- renderPlot({
-      ggplot(mtcars, aes(x = input$selection, y = cyl)) +
+      ggplot(clean_temperature_data, aes(x = date, y = TempC)) +
         theme_classic() + geom_smooth()}, res = 98)
     
     output$testText <- renderText({
