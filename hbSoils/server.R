ad7f262c358879bb6522e040035e8a6bd1320bfa
YourName <your@mail.com>
2022-02-25 13:27:32 -0500

    "Reactive Tabs"

--- hbSoils/server.R
+++ hbSoils/server.R
@@ -8,18 +8,45 @@
 #
 
 
-
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
-
-    
-    output$timePlot <- renderPlot({
-      ggplot(mtcars, aes(x = input$selection, y = cyl)) +
-        theme_classic() + geom_smooth()}, res = 98)
-    
-    output$testText <- renderText({
-      
-    })
-    
-
+  
+  
+  data_selected <- reactive({
+    selectData(input$tabs)
+  })
+  
+  
+  
+  
+  #Reactive data for updating between certain dates
+  data <- reactive(
+    allCleanData %>% filter(between(date, as.POSIXct(input$dateRange[1]),
+                                    as.POSIXct(input$dateRange[2])))
+  )  
+  
+  
+  #Output plot for any selected variable
+  output$timePlot <- renderPlot({
+    ggplot(data_selected(), aes(x = date, !!input$selection)) +
+      theme_classic() + geom_line() + labs(x = "Time",
+                                           title = "Specified Variable over time")}, res = 80)
+  
+  
+  #Output text for DEBUGGING and seeing specific date range
+  output$testText <- renderText({
+    paste0("Date Range is ", input$dateRange)
+  })
+  
+  
+  selectData <- function(name) {
+    if (name == "Soil Data") {
+      return(master_soil)
+    }
+    else if(name == "Aquatic Data") {
+      return(allCleanData)
+    }
+    return(master_soil)
+  }
+  
 })
--- hbSoils/ui.R
+++ hbSoils/ui.R
@@ -7,33 +7,77 @@
 #    http://shiny.rstudio.com/
 #
 #This is a test.
+#add lines
 
 library(shiny)
+library(shinythemes)
+library(shinydashboard)
+library(lubridate)
+library(ggplot2)
+library(tidyverse)
+
+
+setwd("~/S22-Soils-App/")
+
+source("hbSoils/waterCleaningScript.R")
 
 
 shinyUI(fluidPage(
-
-    # Application title
-    titlePanel("Data Vizualization Of Hubbard Brook (Using Tabsets)"),
-
-    # Sidebar with a slider input for number of bins
-    sidebarLayout(
-        sidebarPanel(
-            dateRangeInput("dateRange", "Date Range for Analysis",
-                           start = "2012-11-1",
-                           end = "2013-11-1"),
-            varSelectInput("selection", "Column Name:", mtcars)
-        ),
-       
-
-        
-        
-        
-        
-        # Show a plot of the generated distribution
-        mainPanel(
-            plotOutput("timePlot"),
-            textOutput("testText")
-        )
+  
+  # Application title
+  titlePanel("Data Vizualization Of Hubbard Brook"),
+  
+  # Creates two tabs for timeseries & bivariate data
+  navbarPage("Type of Analysis", theme = shinytheme("flatly"),
+             tabPanel("Bivariate", h2("Bivariate Tab")),
+             navbarMenu("Timeseries", id = "tabs",
+                                    tabPanel("Soil Data",
+                                             h2("Soil Data"), id = "Soil Data"
+                                    ),
+                                    tabPanel("Aquatic Data",
+                                             h2("Aquatic Data"), id = "Aquatic Data"
+                                    )
+                        )),
+  
+  # Sidebar Panel responsible for selecting date Range and Variable
+  sidebarLayout(
+    
+    conditionalPanel(condition="input.tabselected==1",h4("Soil Data"),
+                     dateRangeInput("dateRange", "Desired Date Range",
+                                    start = "2012-11-1",
+                                    end = "2013-11-1"),
+                     varSelectInput("selection", "Variable", master_soil %>% select(
+                     ))
+                     ),
+   
+    
+    
+    conditionalPanel(condition="input.tabselected==2",
+                     selectInput("dataset", "select the desired dataset", choices=ls('package:datasets'), 
+                                 selected = "mtcars"),
+                     radioButtons("choice","Choose an option", choices=c("Dataset" = 1, "Structure" = 2,
+                                                                         "Summary" = 3 ))
+                     
     )
+    
+    sidebarPanel(
+      dateRangeInput("dateRange", "Desired Date Range",
+                     start = "2012-11-1",
+                     end = "2013-11-1"),
+      #Select only columns that we need for variables to look at
+      varSelectInput("selection", "Variable", allCleanData %>% select(TempC,
+                                                                      SpConductivity,
+                                                                      NO3_corrected_mgL,
+                                                                      FDOM_corrected_QSU,
+                                                                      Q_Ls
+      ))
+    ),
+    
+    
+    # Displays timeseries updatable graph and specific date range text
+    mainPanel(
+      plotOutput("timePlot"),
+      textOutput("testText")
+    )
+  )
 ))
